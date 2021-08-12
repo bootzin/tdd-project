e565e09c816fa4de3c642ca15c41b04c5c6f1444
Lincoln Konig <lincolnkonig5@gmail.com>
2021-08-11 22:41:00 -0300

    Integrando o Bootstrap

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -7,7 +7,7 @@ import unittest
 
 MAX_WAIT = 5
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def test_layout_and_styling(self):
         # Edith entra na home page
         self.browser.get(self.live_server_url)
